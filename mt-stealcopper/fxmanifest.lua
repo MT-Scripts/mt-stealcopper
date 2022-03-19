23704fd24e6ce220cf961739de1fcf4e18151618
MT Scripts <89866234+Marttins011@users.noreply.github.com>
2022-03-19 19:22:49 +0000

    Add script files

---
+++ mt-stealcopper/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'mt scripts'
+
+description 'mt-stealcopper'
+
+version '0.1'
+
+client_scripts{
+    'client.lua',
+}
+
+server_scripts{
+    'server.lua',
+}
